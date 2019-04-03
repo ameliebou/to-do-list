3b309d43581988fa99ff4eb94660b77bffed1b15
Amelie Boudier <amelie.boudier01@gmail.com>
2019-04-03 18:23:32 +0200

    added a done state to task

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
 class Task extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    }
+  }
+
+  handleClick = (event) => {
+    this.setState({
+      done: !this.state.done
+    })
+  }
+
   render() {
     return (
-      <div className="task">
+      <div className={this.state.done? "task done" : "task"} onClick={this.handleClick}>
         <div className="task-title">
           <h3>{this.props.title}</h3>
         </div>
